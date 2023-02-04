126ad0e2e82ff5f47d368165e4061e2ffe36ba29
Sandro Park <ticoba@naver.com>
2023-02-04 14:38:45 +0900

    자료구조 - ArrayList : add 리펙토링

--- data_structure/src/main/java/list/arraylist/implement/ArrayList.java
+++ data_structure/src/main/java/list/arraylist/implement/ArrayList.java
@@ -19,14 +19,6 @@ public class ArrayList {
     }
 
     public void add(int idx, Object element) {
-        // 해당 인덱스가 마지막이라면
-        if (idx == size) {
-            elementData[idx] = element;
-            size++;
-            return;
-        }
-
-        // 해당 인덱스가 요소 사이라면
         // 1. 해당 인덱스의 요소부터 뒤의 요소들의 인덱스를 1씩 증가시킨다.
         for (int i = size-1; i >= idx; i--) {
             elementData[i + 1] = elementData[i];
