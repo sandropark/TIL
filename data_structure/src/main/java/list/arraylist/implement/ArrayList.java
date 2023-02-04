9868f1aaf72b010805e8c046630a8643914fc7b6
Sandro Park <ticoba@naver.com>
2023-02-04 14:13:12 +0900

    자료구조 - ArrayList : size 구현

--- data_structure/src/main/java/list/arraylist/implement/ArrayList.java
+++ data_structure/src/main/java/list/arraylist/implement/ArrayList.java
@@ -1,4 +1,11 @@
 package list.arraylist.implement;
 
 public class ArrayList {
+    public static final int INITIAL_SIZE = 100;
+    private int size;
+    private Object[] elementData = new Object[INITIAL_SIZE];
+
+    public int length() {
+        return size;
+    }
 }
--- data_structure/src/test/java/list/arraylist/implement/ArrayListTest.java
+++ data_structure/src/test/java/list/arraylist/implement/ArrayListTest.java
@@ -1,5 +1,22 @@
 package list.arraylist.implement;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class ArrayListTest {
 
+    ArrayList numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new ArrayList();
+    }
+    @DisplayName("리스트에 포함된 요소갯수를 반환한다.")
+    @Test
+    void length() throws Exception {
+        assertThat(numbers.length()).isEqualTo(0);
+    }
 }
