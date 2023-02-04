c6fc470940015f6d14afeb6aaef558738394f6e6
Sandro Park <ticoba@naver.com>
2023-02-04 14:30:21 +0900

    자료구조 - ArrayList : add

--- data_structure/src/main/java/list/arraylist/implement/ArrayList.java
+++ data_structure/src/main/java/list/arraylist/implement/ArrayList.java
@@ -18,4 +18,22 @@ public class ArrayList {
         return elementData[idx];
     }
 
+    public void add(int idx, Object element) {
+        // 해당 인덱스가 마지막이라면
+        if (idx == size) {
+            elementData[idx] = element;
+            size++;
+            return;
+        }
+
+        // 해당 인덱스가 요소 사이라면
+        // 1. 해당 인덱스의 요소부터 뒤의 요소들의 인덱스를 1씩 증가시킨다.
+        for (int i = size-1; i >= idx; i--) {
+            elementData[i + 1] = elementData[i];
+        }
+        // 2. 해당 인덱스에 입력받은 요소를 넣는다.
+        elementData[idx] = element;
+        size++;
+    }
+
 }
--- data_structure/src/test/java/list/arraylist/implement/ArrayListTest.java
+++ data_structure/src/test/java/list/arraylist/implement/ArrayListTest.java
@@ -36,4 +36,34 @@ class ArrayListTest {
         assertThat(numbers.get(0)).isEqualTo(10);
     }
 
+    @DisplayName("원하는 인덱스에 값 넣기 : 가장 마지막에 요소 추가")
+    @Test
+    void add() throws Exception {
+        numbers.addLast(10);
+        numbers.addLast(20);
+        numbers.addLast(30);
+
+        numbers.add(3, 40);
+
+        assertThat(numbers.length()).isEqualTo(4);
+        assertThat(numbers.get(3)).isEqualTo(40);
+    }
+
+    @DisplayName("원하는 인덱스에 값 넣기 : 요소들 사이에 값을 넣는다.")
+    @Test
+    void add2() throws Exception {
+        numbers.addLast(10);
+        numbers.addLast(20);
+        numbers.addLast(30);
+
+        numbers.add(1, 40);
+
+        assertThat(numbers.length()).isEqualTo(4);
+
+        assertThat(numbers.get(0)).isEqualTo(10);
+        assertThat(numbers.get(1)).isEqualTo(40);
+        assertThat(numbers.get(2)).isEqualTo(20);
+        assertThat(numbers.get(3)).isEqualTo(30);
+    }
+
 }
