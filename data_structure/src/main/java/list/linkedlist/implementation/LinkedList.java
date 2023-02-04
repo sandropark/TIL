9dbf09e4654e8c6a3bfcb436280497bad090bc93
Sandro Park <ticoba@naver.com>
2023-02-04 19:46:54 +0900

    자료구조 - LinkedList : addFirst

--- data_structure/src/main/java/list/linkedlist/implementation/LinkedList.java
+++ data_structure/src/main/java/list/linkedlist/implementation/LinkedList.java
@@ -1,8 +1,30 @@
 package list.linkedlist.implementation;
 
 public class LinkedList {
+
+    private Node head;
+    private Node tail;
     private int size;
+
+    private class Node {
+
+        private Object value;
+        private Node next;
+        public Node(Object input, Node next) {
+            value = input;
+            this.next = next;
+        }
+    }
     public int length() {
         return size;
     }
+
+    public void addFirst(Object input) {
+        head = new Node(input, head);
+        size++;
+        if (head.next == null) {
+            tail = head;
+        }
+    }
+
 }
--- data_structure/src/test/java/list/linkedlist/implementation/LinkedListTest.java
+++ data_structure/src/test/java/list/linkedlist/implementation/LinkedListTest.java
@@ -1,5 +1,6 @@
 package list.linkedlist.implementation;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,12 +8,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class LinkedListTest {
 
+    LinkedList numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new LinkedList();
+    }
+
     @DisplayName("리스트 요소의 갯수를 반환한다.")
     @Test
     void length() throws Exception {
-        LinkedList numbers = new LinkedList();
-
         assertThat(numbers.length()).isEqualTo(0);
+    }
+
+    @DisplayName("리스트 가장 앞에 요소를 추가한다.")
+    @Test
+    void addFirst() throws Exception {
+        numbers.addFirst(30);
+        numbers.addFirst(20);
+        numbers.addFirst(10);
+
+        assertThat(numbers.length()).isEqualTo(3);
     }
 
 }
